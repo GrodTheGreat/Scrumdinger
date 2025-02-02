eb6f78df03a8e5d818a6b860d1a86333dbeb5688
Gerard Causse <grod.causse@gmail.com>
2025-02-02 00:44:12 -0500

    Display list of daily scrums

---
+++ Scrumdinger/Scrums.swift
@@ -0,0 +1,24 @@
+//
+//  Scrums.swift
+//  Scrumdinger
+//
+//  Created by Gerard Causse on 2/2/25.
+//
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Preview: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
